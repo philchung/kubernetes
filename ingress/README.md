c9b0417c26eef9bd4e7e88b48de97dff0742ba03
Peter Nagy <nagypeter76@gmail.com>
2017-11-22 19:25:16 +0100

    all yaml complete

---
+++ ingress/README.md
@@ -0,0 +1,5 @@
+# Setup Ingress controller
+
+You can run directly the yaml definitions from github.
+
+	curl -LSs https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/create-ingress.sh | sh
--- ingress/create-ingress.sh
+++ ingress/create-ingress.sh
@@ -2,14 +2,17 @@
 
 export NAMESPACE=shared-ingress
 
+# wait to delet resources
+sleep 15
+
 kubectl delete namespace $NAMESPACE --ignore-not-found=true
 
 kubectl create namespace $NAMESPACE
 
 kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-deployment.yaml -n=$NAMESPACE
 
-#kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-service.yaml
+kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-service.yaml -n=$NAMESPACE
 
-#kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-deployment.yaml -n=$NAMESPACE
 
-#kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-service.yaml
+kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-service.yaml -n=$NAMESPACE
