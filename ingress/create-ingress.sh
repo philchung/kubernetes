4110ead086127c43ad83cece3baa53f76734ca67
Peter Nagy <nagypeter76@gmail.com>
2017-11-22 19:10:14 +0100

    create-ingress.sh

---
+++ ingress/create-ingress.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-deployment.yaml
+
+#kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-service.yaml
+
+#kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-deployment.yaml
+
+#kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-service.yaml
